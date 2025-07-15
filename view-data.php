57211b469afd33d3ca5ff04262a8524d0c3df652
chwmq <chwmqwork@outlook.com>
2025-07-15 16:02:18 +0800

    Update view-data.php

--- view-data.php
+++ view-data.php
@@ -1,4 +1,10 @@
 <!--用于查看访问数据-->
+<!--
+* Website-data-statistics:用于统计网站的访问数据
+* Copyright (c) 2010 - 2025 chwmq. All rights reserved.
+* github: https://github.com/chwmq/
+* csdn: https://blog.csdn.net/chwmq
+-->
 <?php include 'data.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
